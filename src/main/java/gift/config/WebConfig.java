b5f0377e0716e6e3fae3e1c816d3e9111048a985
Junhee <dkfl7190@naver.com>
2025-08-01 17:18:53 +0900

    feat: CORS 설정 추가
    - 현재 프론트엔드 클라이언트가 없는 상황이므로, 일반적인 개발 환경인 `http://localhost:3000`을 허용 출처로 가정하여 설정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    public static final String ALLOWED_METHOD_NAMES = "GET,POST,PUT,DELETE,PATCH,OPTIONS";
 
     @Bean
     public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter(
@@ -25,5 +29,14 @@ public class WebConfig implements WebMvcConfigurer {
         registrationBean.addUrlPatterns("/api/orders/*");
 
         return registrationBean;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION)
+                .allowCredentials(true);
     }
 }
